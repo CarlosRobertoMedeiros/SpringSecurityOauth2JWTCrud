05cc77e31e2486af85d7865535ee6128e2a63301
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-07-30 07:10:09 -0300

    Implementando a funcionalidade de Refresh_Token

--- src/main/java/com/robertodev/springsecuritycrud/SpringSecurityCrudApplication.java
+++ src/main/java/com/robertodev/springsecuritycrud/SpringSecurityCrudApplication.java
@@ -13,9 +13,13 @@ public class SpringSecurityCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityCrudApplication.class, args);
+		//Refazer a aula
+			//6.7. Movendo o refresh token para o cookie
+		
+			
 		//Realizando a Autenticação básica
 		//Curso
-		// 6.1. Implementando autenticação Oauth2 Com Bearer
+		// 6.1. Implementando autenticação Oauth2 Com JWTAccessToken Bearer
 		/*RegraBásica
 		
 			Criar as Classes 
@@ -27,13 +31,16 @@ public class SpringSecurityCrudApplication {
 				- configure(ClientDetailsServiceConfigurer clients)
 				- configure(AuthorizationServerEndpointsConfigurer endpoints)
 					Aqui informa em memória, usuário e senha
+					Acresscenta um conversor para jwt
+					IMplementa o conversor
 				- configure(HttpSecurity http)
 					Aqui informa as configurações Stateless
 					Quais apis são permitidas e para quem
 			
 			Para o ResourceServerConfig
 				configure(AuthenticationManagerBuilder auth) 
-				configure(HttpSecurity http) 
+				configure(HttpSecurity http)
+				 
 				
 			Para o Postman
 				Criar uma requisição localhost:8080/oauth/token(POST)
@@ -49,6 +56,16 @@ public class SpringSecurityCrudApplication {
 					Preview Request
 			
 				Ao chamar a função, informar bearer AccessToken Result
+
+			O REFRESH_TOKEN é atualizado sempre com intervalo de 1 dia
+			enquanto o ACCESS_TOKEN de 1 minuto por exemplo
+			Isso quer dizer que cada vez que eu pedir um novo access token vai ser gerado um novo 
+			Refresh Token, garantindo assim que ele sempre tem brecha de 1 dia
+			
+			 Para garantir que ninguém tem acesso ao refresh tokem ele deve ser colocado em um 
+			 cookie seguro ... Assim as chamadas Javascript não vão interferir nele.
+			 
+			
 			*/
 	}
 }
--- src/main/java/com/robertodev/springsecuritycrud/config/AuthorizationServerConfig.java
+++ src/main/java/com/robertodev/springsecuritycrud/config/AuthorizationServerConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * @author roberto
@@ -32,28 +33,40 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		//Dado em Memória poderia ser no banco de dados
-		final Integer MINUTOS = 30;
+		final Integer MINUTOS = 1;
 		final Integer SEGUNDO = 60;
+		
+		
 		
 		
 		clients.inMemory()
 				.withClient("angular")
 				.secret("@angular0")
 				.scopes("read","write")
-				.authorizedGrantTypes("password") //GrantType = passwordFlow
-				.accessTokenValiditySeconds(SEGUNDO * MINUTOS);
+				.authorizedGrantTypes("password","refresh_token") //GrantType = passwordFlow 
+																  //O RefreshToken é usado para garantir segurança
+				.accessTokenValiditySeconds(SEGUNDO * MINUTOS)
+				.refreshTokenValiditySeconds(3600 * 24);
 	}
-	
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
+		.reuseRefreshTokens(false) 
 		.authenticationManager(autenticationManager);
 	}
 	
 	@Bean
+	protected JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("adminserver");
+		return accessTokenConverter;
+	}
+
+	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 	
 }
